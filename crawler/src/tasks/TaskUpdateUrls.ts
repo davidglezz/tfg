17b1130912742aa877d07b23c03f6adc860a0d4d
davidglezz <davidglezz@outlook.es>
2017-10-31 00:20:58 +0100

    Clean crawler TaskUpdateUrls.ts

--- crawler/src/tasks/TaskUpdateUrls.ts
+++ crawler/src/tasks/TaskUpdateUrls.ts
@@ -19,9 +19,9 @@ export class TaskUpdateUrls implements Task {
     private config = {
         BatchSize: 1000,
         Concurrency: 25,
-        RedirectUpdTime: 31536000000, // 1 año
-        NoProductUpdTime: 31536000000, // 1 año
-        DefaultTimeToNextRun: 60000 // 1 minuto
+        RedirectUpdTime: 31536000000, // 1 year
+        NoProductUpdTime: 31536000000, // 1 year
+        DefaultTimeToNextRun: 60000 // 1 minute
     }
 
     constructor(public stats: Dictionary<any> = {}, config?: any) {
@@ -40,11 +40,11 @@ export class TaskUpdateUrls implements Task {
     }
 
     /**
-     * Analiza urls en busca de microdatos
+     * Analyze urls in search of microdata
      */
     async run() {
         this.stats.runCount++
-        //console.log('[-] Tarea de actualizacion de url, productos y precios')
+        console.info(`[RUN] ${TaskUpdateUrls.name}`)
         return new Promise<void>(async (resolve, reject) => {
             let result: Url[] = await this.repository.url.createQueryBuilder("url")
                 .innerJoinAndSelect("url.shop", "shop")
@@ -56,92 +56,91 @@ export class TaskUpdateUrls implements Task {
 
             eachLimit<Url, Error>(result, this.config.Concurrency, async (url: Url) => {
                 this.stats.proccessedUrls++
-                await this.proccessUrl(url).catch(e => console.error("[ERROR] Al procesar", url, e))
+                await this.proccessUrl(url).catch(e => console.error("[ERROR] Processing", url, e))
             }, async (err: any) => {
                 if (err)
-                    console.warn(err) // no se hace reject(err), No pasa nada
+                    console.warn(err) // no reject(err), if error, continue
 
-                // Resolver inmediatamente o // DevMode:
+                // Resolve immediately
                 resolve() // setTimeout(resolve, await this.getNextUpdate())
             })
         })
     }
 
     /**
-     * Extrae los microdatos de la url y actualiza los registros correspondientes
+     * Extract microdata from the url and update the corresponding records
      * @param url 
      */
     async proccessUrl(url: Url) {
-        // TODO detección temprana de redirecciones .. request.on('redirect')
+        // TODO early redirection detection. request.on('redirect')
         let response = await extractMicrodata(url.href)
-            .catch(() => console.info('No contiene o no se pudieron obtener microdatos:', url.href))
+            .catch(() => console.info('Does not contain or could not obtain microdata:', url.href))
 
-        // TODO definir estados
+        // TODO define states
         url.status = !response || !response.status ? 600 : response.status
 
-        // Si no se pueden obtener los microdatos...
+        // If microdata can not be obtained ...
         if (!response || !response.data) {
-            console.info('No contiene microdatos:', url.href)
+            console.info('Does not contain microdata:', url.href)
             url.dateNextUpd = new Date(Date.now() + this.config.RedirectUpdTime)
             return this.repository.url.save(url).catch(console.error)
         }
 
-        // Si redirecciona no escanear en 1 año y añadir la nueva url
+        // If you redirect do not scan in 1 year and add the new url
         if (response.url != url.href) {
-            console.info("Redireccion", url.href + ' -> ' + response.url)
+            console.info("Redirection", url.href + ' -> ' + response.url)
             url.status = 301
-            // Comprobar si ya existe
+            // Check if it already exists
             let urlHash = Url.getHashCode(response.url)
             let dbUrl = await this.repository.url.findOne({ 'where': { 'hash': urlHash, 'shopId': url.shop.id } })
             url.dateNextUpd = new Date(Date.now() + this.config.RedirectUpdTime)
             const urls = [url]
             if (!dbUrl)
                 urls.push(new Url(response.url, url.shop, undefined, urlHash))
-            // let newUrl = this.repository.url.create({ hash: urlHash, shop: url.shop, href: response.url })
             return this.repository.url.save(urls).catch(console.error)
         }
 
-        // Si la página no contiene solo un producto
+        // If the page does not contain only one product
         const nbProducts = response.data ? numberOfProducts(response.data) : 0
         if (nbProducts != 1) {
-            // No volver a analizar hasta dentro de bastante tiempo
-            console.info(`Contiene ${nbProducts} productos != 1`, url.href)
+            // Do not re-analyze for a long time
+            console.info(`It contains ${nbProducts} products != 1`, url.href)
             url.dateNextUpd = new Date(Date.now() + this.config.NoProductUpdTime)
             return this.repository.url.save(url).catch(console.error)
         }
 
-        // La página contiene un solo producto:
+        // The page contains only one product:
         let productInfo: ProductDTO = productFromMetadata(response.data as Result)
         let priceChanges = true
 
         this.calcTotalPrice(productInfo, url.shop)
 
-        // Si el producto no esta creado: Crear producto y establecer precio
+        // If the product is not created in db: Create and set price
         if (!url.product) {
-            console.info("Crea producto", url.href)
+            console.info("New product", url.href)
             url.product = this.repository.product.create(productInfo)
             await this.repository.product.save(url.product).catch(console.error)
         } else {
-            // Si el precio o disponibilidad cambia
-            priceChanges = Math.abs(url.product.price - productInfo.price) >= 0.01 // Diferencia >= a 1 céntimo
+            // If the price or availability changes
+            priceChanges = Math.abs(url.product.price - productInfo.price) >= 0.01
             if (priceChanges || url.product.availability !== productInfo.availability) {
-                console.info('Actualiza precio:', url.href)
+                console.info('Price does not update:', url.href)
                 url.product.price = productInfo.price
                 url.product.priceTotal = productInfo.priceTotal
                 url.product.availability = productInfo.availability
                 await this.repository.product.save(url.product).catch(console.error)
             } else {
-                console.info('No actualiza precio:', url.href)
+                console.info('Price does not update:', url.href)
             }
         }
 
-        // Guardar url
+        // Save url
         url.dateNextUpd = new Date(Date.now() + url.shop.productUpdInterval)
         await this.repository.url.save(url).catch((e) => {
-            console.error("Ocurrio un error al guardar url.", url.href)
+            console.error("An error occurred while saving url.", url.href)
         })
 
-        // Añadir historial de precio
+        // Add price history
         if (priceChanges && productInfo.price >= 0) {
             let priceHistory = this.repository.priceHistory.create({
                 date: timestampToSql(Date.now()),
@@ -173,8 +172,8 @@ export class TaskUpdateUrls implements Task {
     }
 
     /**
-     * Calcula el precio total del producto.
-     * Esto deberia mejorarse teniendo en cuenta otros factores (peso, pais, ...)
+     * Calculate the estimated total price of the product.
+     * This should be improved taking into account other factors (weight, country, ...)
      * @param productInfo
      * @param shop
      */
