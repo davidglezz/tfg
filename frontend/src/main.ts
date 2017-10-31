e5e26eb050f99a863020a7036a45761a9c8e8edc
davidglezz <davidglezz@outlook.es>
2017-10-31 23:56:38 +0100

    Add i18n aot builds

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,32 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { getTranslationProviders } from './app/i18n-providers';
-
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-
-/**
- * JIT compile.
- */
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.module';
-// platformBrowserDynamic().bootstrapModule(AppModule);
+import { platformBrowser } from '@angular/platform-browser';
+platformBrowser().bootstrapModule(AppModule);
 // .then(success => console.log(`Bootstrap success`)).catch(err => console.error(err));
-getTranslationProviders().then(providers => {
-  console.log(providers);
-  const options = { providers };
-  platformBrowserDynamic().bootstrapModule(AppModule, options);
-});
-
-/**
- * AoT compile.
- * First run `./node_modules/.bin/ngc -p ./src/`
- */
-// import { platformBrowser } from '@angular/platform-browser';
-// import { MaterialAppModuleNgFactory } from './aot/app/app.module.ngfactory';
-// platformBrowser().bootstrapModuleFactory(MaterialAppModuleNgFactory);
-
-
